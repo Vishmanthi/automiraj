0f56dc14b49f582c54fa9dd6659f005183d34ece
Sasini VD <37654454+sasinivd@users.noreply.github.com>
2018-09-24 17:37:17 +0530

    customer dashboard and Customer controller
    customer dashboard and Customer controller

---
+++ application/views/customer_header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Auto Miraj</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="<?php echo base_url(); ?>/assests/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+.mukta{
+  font-family: "Mukta", sans-serif;
+}
+.mont{
+  font-family: "Montserrat", sans-serif;
+}
+.buttonop:hover{opacity:0.5;}
+</style>
+</head>
+<body>
+	<!-- Navbar -->
+	<div class="w3-top">
+	  <div class="w3-bar w3-card w3-black" style="opacity: 0.8">
+	    <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right buttonop" href="javascript:void(0)" onclick="navigation()" title="Toggle Navigation Menu" style="padding: 13px 20px"><i class="fa fa-bars"></i></a>
+	    <a class="w3-bar-item w3-button w3-hide-small w3-hide-large w3-right buttonop" href="javascript:void(0)" onclick="navigation1()" title="Toggle Navigation Menu" style="padding: 13px 20px"><i class="fa fa-bars"></i></a>
+	    <img class="w3-bar-item" style="padding: 0px 0px;height:45px;width: 209px" src="<?php echo base_url(); ?>/assests/images/logo2.png">
+	    <div class="w3-right">
+	      <a href="#" class="w3-bar-item w3-button w3-hover-yellow w3-hide-small " style="padding: 13px 16px">HOME</a>
+	      <a href="#about" class="w3-bar-item w3-button  w3-hide-small w3-hover-yellow w3-hide-medium" style="padding: 13px 16px">ABOUT US</a>
+	      <a href="#contact" class="w3-bar-item w3-button  w3-hide-small w3-hover-yellow w3-hide-medium" style="padding: 13px 16px">SERVICES</a>
+	      <a href="#contact" class="w3-bar-item w3-button  w3-hide-small w3-hover-yellow" style="padding: 13px 16px">CONTACT</a>
+	      <a href="#contact" class="w3-bar-item w3-button w3-hide-small w3-hover-yellow w3-hide-medium" style="padding: 13px 16px">PROMOTIONS</a>
+	      <a onclick="document.getElementById('login').style.display='block'" class="w3-bar-item w3-button w3-hover-yellow w3-hide-small" style="padding: 13px 16px;width: auto;">LOGOUT</a>
+	      
+	    </div>
+	  </div>
+	</div>
+
+	<!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
+	<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+	  <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="navigation()">HOME</a>
+	  <a href="#about" class="w3-bar-item w3-button w3-padding-large" onclick="navigation()">ABOUT US</a>
+	  <a href="#tour" class="w3-bar-item w3-button w3-padding-large" onclick="navigation()">SERVICES</a>
+	  <a href="#contact" class="w3-bar-item w3-button w3-padding-large" onclick="navigation()">CONTACT</a>
+	  <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="navigation()">PROMOTIONS</a>
+	  <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="navigation()">LOGIN</a>
+	</div>
+
+	<!-- Navbar on tablets,medium screens -->
+	<div id="navDemoM" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-small w3-top" style="margin-top:46px">
+	  <a href="#about" class="w3-bar-item w3-button w3-padding-large" onclick="navigation1()">ABOUT US</a>
+	  <a href="#tour" class="w3-bar-item w3-button w3-padding-large" onclick="navigation1()">SERVICES</a>
+	  <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="navigation1()">PROMOTIONS</a>
+	</div>
+</body>
+</html>
---
+++ application/views/customer_sidebar.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {
+  margin: 0;
+  font-family: "Lato", sans-serif;
+}
+
+.sidebar {
+  margin: 0px;
+  padding: 0;
+  width: 200px;
+  background-color: rgba(255,255,255,0.6);
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+.bg-image {
+  background-image: url("<?php echo base_url(); ?>/assests/images/carcare4.jpg");
+  width: 200px;
+}
+
+.sidebar a {
+  display: block;
+  color:black;
+  padding: 34px 30px ;
+  text-decoration: none;
+  cursor: pointer;
+  transition: 0.6s;
+  border-bottom: 1px solid #ddd;
+
+}
+ 
+.sidebar a.active {
+  background-color: rgb(0,0,0);
+  color: white;
+}
+
+.sidebar a:hover:not(.active) {
+  background-color: rgba(255,234,0,0.5);;
+  border-radius: 8px;
+
+  color: black;
+}
+
+div.content {
+  margin-left: 200px;
+  background-color: white;
+  padding: 1px 16px;
+  height: 670px;
+}
+
+
+
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar a {float: left;}
+  div.content {margin-left: 0;}
+}
+
+@media screen and (max-width: 400px) {
+  .sidebar a {
+    text-align: center;
+    float: none;
+  }
+}
+</style>
+</head>
+<body>
+  <div class="w3-content" style="max-width:2000px;margin-top:49px;">
+    
+    <div class="sidebar" >
+      <div class="w3-center">
+        <a href="../customer/editProfile" class=" w3-hover-shadow"><img src="<?php echo base_url(); ?>/assests/images/user.png"><br>Edit Profile</a>
+              
+          <a href="../customer/reserveService" class=" w3-hover-shadow"><img src="<?php echo base_url(); ?>/assests/images/add2.png"><br>Reserve Service</a>
+          
+          
+          <a href="../customer/serviceHistory" class=" w3-hover-shadow"><img src="<?php echo base_url(); ?>/assests/images/car2.png"><br>Service History</a>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
