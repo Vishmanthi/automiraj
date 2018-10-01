37a309f590eef841ca928ec3af945ae05d436c4b
Sasini VD <37654454+sasinivd@users.noreply.github.com>
2018-10-01 17:01:53 +0530

    encripted_pass

--- application/models/customer_model.php
+++ application/models/customer_model.php
@@ -56,6 +56,7 @@ class Customer_model extends CI_Model{
 		$this->db->where('nic',$this->input->post('nic'));
 		//$result=$this->db->get('customer');
 		$id=$this->db->get('customer')->row(0)->id;
+		$encripted_pass=password_hash($this->input->post('password'),PASSWORD_BCRYPT);
 		$d2=array(
 			'id'=>$id,
 			'username'=>$this->input->post('first_name'),
